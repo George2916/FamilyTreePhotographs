78ca4b05d849878e469b80921c48617b21fef7da
George Greenslade <ggreenslade94@gmail.com>
2023-07-02 14:45:03 -0400

    July 2, 2023 commit

--- js/myJavascript.js
+++ js/myJavascript.js
@@ -27,3 +27,26 @@ $("div[id^='myModal']").each(function(){
   function myFunction2() {
     location.href = "gallery1.html"
   }
+
+/* javascript for visitor counter at bottom of index.html */
+
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
